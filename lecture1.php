d189fcf09bb1e4acedfa382e0de513f5f0456580
Jakub Tucholski <jtucholski00@gmail.com>
2015-03-12 20:31:32 -0500

    adding stuff

--- lecture1.php
+++ lecture1.php
@@ -15,9 +15,23 @@
 
 <p>While there is a growing field of Haskell writing that focuses on writing software meant to exist in the wild, much of this writing is scattered across various blog posts that occasionally bubble up in Hacker News. As such, our secondary, and much more ambitious goal, is to create some kind of unified resource for advanced, systems-driven programming in Haskell. We hope that this class can begin serving this end. Due to the experimental nature of this, we'd like to come into this with the expectation that much of this material is (at best) in beta mode, from both the pedagogical and software perspective. There will be rough patches, things will break, and we will get things wrong. Come into this class with a tolerance for these problems, and we will do our best to reward your patience.</p>
 
+<h2>A Gentle Start</h2>
+
+<p>Let's begin with the basics. Haskell is a strong, statically typed, pure functional language that is lazily evaluated. This pretty much covers most of what you need to know, but we'll spend the better part of this lecture unraveling what these statements mean.</p>
+
+<p>Let's start with some basic computations and move into the type system. Let's jump into some examples. For these, let's boot up GHCi.</p>
+<code>
+> let height = 5
+> let width = 10
+> width * height
+50
+> width - height
+5
+</code>
+<p>Nothing shocking here: we can perform basic calculations
 <h2>Hitting the Ground Running</h2>
 <p>I'm of the belief that good problems get us to great solutions, so we're going to begin with a classic: the <strong><a href="#">maximum subarray problem</a></strong>. Formally stated, (ripped directly from Wikipedia), "the maximum subarray problem is the task of finding the contiguous subarray within a one-dimentional array of numbers (containing at least one positive number) which has the largest sum."</p>
 
-<p>Our task is going to be to use what we've learned thus far to write a function that calculates and returns the maximum sum subarray given a list of numbers. By the end of this lecture, we'll have created </p>
+<p>Our task is going to be to use what we've learned thus far to write a function that calculates and returns the maximum sum subarray given a list of numbers. By the end of this lecture, we'll have created a command line tool that takes a in a text file as input, finds the max-sum subarray of each line, and outputs each of these subarrays to a file.</p>
 
 <?php foot(); ?>
