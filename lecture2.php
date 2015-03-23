8aed288b55edf22d5170cda9bad63c9eb728e2f0
Jakub Tucholski <jtucholski00@gmail.com>
2015-03-22 23:15:15 -0500

    lecture 2 php

---
+++ lecture2.php
@@ -0,0 +1,5 @@
+<php>
+<h1>Typeclassopaedia</h1>
+<p>Haskell is has a lot in common with modern philosophy. You're more likely to find its adherents in the halls of academia than you are to find them in the wild. The ideas you will come upon while studying Haskell are incomprehensible to most and difficult to master, but they are hugely influential, and becoming more so with the rise of JVM-based functional languages, primarily Scala and Clojure.</p>
+<p>Today, we're going to focus on one of the foundational texts in Haskell literature: the Typeclassopaedia. The Typeclassopedia was initially written as a paper by Brett Yorgej, a graduate student at UPenn. It covers</p>
+</php>
